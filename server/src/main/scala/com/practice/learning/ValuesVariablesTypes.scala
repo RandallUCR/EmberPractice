52809186efdadbcfb399def93fa714cf37ab4e88
RandallUCR <randallgears1998@gmail.com>
2022-01-12 18:19:45 -0600

    scala init

---
+++ server/src/main/scala/com/practice/learning/ValuesVariablesTypes.scala
@@ -0,0 +1,35 @@
+package com.practice.learning
+
+object ValuesVariablesTypes extends App {
+  var x = 12 //val for static
+  //var x: Int = 12
+  println(x)
+
+  var b = true
+  //var b: Boolean = true
+  println(b)
+
+  var c = 'a' //simple quotes for char
+  //var c: Char = 'a'
+  println(c)
+
+  var s = "hello" //double quotes for string
+  //var s: String = "hello"
+  println(s)
+
+  var y = 9999 // 4 bits
+  //var y: Short = 9999
+  println(y)
+
+  var l = 999999999999999999L // 8 bits
+  //var l: Long = 999999999999999999L
+  println(l)
+
+  var f = 2.013f
+  //var f: Float = 2.013f
+  println(f)
+
+  var d = 2.013
+  //var d: Double = 2.013
+  println(d)
+}
