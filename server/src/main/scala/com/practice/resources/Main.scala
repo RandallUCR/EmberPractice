75fb6d395ec58a25e5fb2c3cc7f2e58c6969f52d
RandallUCR <randallgears1998@gmail.com>
2022-01-19 12:37:34 -0600

    Dummy REST API

--- server/src/main/scala/com/practice/resources/Main.scala
+++ server/src/main/scala/com/practice/resources/Main.scala
@@ -25,11 +25,45 @@ object Main extends App with Endpoint.Module[IO] {
     Ok(List(Class(3, "Math"), Class(7, "Calc"), Class(1, "Spanish")).asJson)
   }
 
+  //Courses example
+
+  case class Course(id: Int, room: Int, name: String)
+
+  var courses = Array(Course(0, 35, "Maths"), Course(1, 30, "Calc"), Course(2, 40, "Spanish"))
+
+  val getCourses: Endpoint[IO, Json] = get("courses") {
+    Ok(courses.asJson)
+  }
+
+  val getCourse: Endpoint[IO, Json] = get("course" :: path[Int]) { id: Int =>
+    Ok(courses(id).asJson)
+  }
+
+  val addCourse: Endpoint[IO, Json] = post("course" :: jsonBody[Course]) { course: Course =>
+    courses = courses :+ course
+    Ok(courses.asJson)
+  }
+
+  val editCourse: Endpoint[IO, Json] = patch("course" :: jsonBody[Course]) { course: Course =>
+    courses(course.id) = course
+    Ok(courses.asJson)
+  }
+
+  val delCourse: Endpoint[IO, Json] = delete("course" :: path[Int]) { id: Int =>
+    courses(id) = Course(-1, -1, " ")
+    Ok(courses.asJson)
+  }
+
   val api: Service[Request, Response] =
     Bootstrap.configure(includeDateHeader = true, includeServerHeader = true)
       .serve[Text.Plain](sayHi)
       .serve[Application.Json](jsonList)
       .serve[Application.Json](classList)
+      .serve[Application.Json](getCourses)
+      .serve[Application.Json](getCourse)
+      .serve[Application.Json](addCourse)
+      .serve[Application.Json](editCourse)
+      .serve[Application.Json](delCourse)
       .toService
 
   Await.ready(Http.server.serve(":8080", api))
